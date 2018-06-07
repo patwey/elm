06dcd798bf3c9085acbae148bd6cc99d2ae61c24
Pat Wey <patrickwey@gmail.com>
2018-06-06 18:19:14 -0700

    Remove comments and run elm-format on hello world solution

--- hello-world/HelloWorld.elm
+++ hello-world/HelloWorld.elm
@@ -1,33 +1,16 @@
-{-
-   This is a "stub" file.  It's a little start on your solution. It's not a
-   complete solution though; you have to write some code.
-
-   The module name is expected by the test program and must match the name of this
-   file. It has to stay just the way it is.
--}
-
-
 module HelloWorld exposing (helloWorld)
-
--- It's good style to include any types at the top level of your modules.
 
 
 helloWorld : Maybe String -> String
 helloWorld name =
     let
-      greetee : String
-      greetee =
-        case name of
-          Just n ->
-            n
+        greetee : String
+        greetee =
+            case name of
+                Just n ->
+                    n
 
-          Nothing ->
-            "World"
+                Nothing ->
+                    "World"
     in
-      "Hello, " ++ greetee ++ "!"
-
-{-
-   When you have a working solution, REMOVE ALL THE STOCK COMMENTS.
-   They're here to help you get started but they only clutter a finished solution.
-   If you leave them in, nitpickers will protest!
--}
+        "Hello, " ++ greetee ++ "!"
